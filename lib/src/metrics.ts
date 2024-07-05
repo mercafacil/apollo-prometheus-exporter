6b7654ee5dd6d5b4392800b0a80ed77602cce136
Lourivaldo Vasconcelos <lourivaldovasconcelos@gmail.com>
2024-07-05 13:31:32 -0300

    feat: custom labels

--- lib/src/metrics.ts
+++ lib/src/metrics.ts
@@ -95,7 +95,7 @@ export interface MetricConfig {
 
 export const serverLabelNames = ['version'];
 
-export const queryLabelNames = ['operationName', 'operation'];
+export const queryLabelNames = ['operationName', 'operation', 'accountid'];
 
 export const fieldLabelNames = ['operationName', 'operation', 'fieldName', 'parentType', 'returnType', 'pathLength'];
 
